d39c6287744a18cee945d71031586d9edac76d18
namhyeonuk <jwvg0425@naver.com>
2015-08-31 14:46:48 +0900

    update solution

--- 2/solution_tuple.hs
+++ 2/solution_tuple.hs
@@ -1,3 +1,8 @@
 swap xs = [(snd x, fst x) | x <- xs]
+-- using pattern matching (see study 4)
+swap' xs = [(y,x) | (x,y) <- xs]
 sum' xs = if null xs then 0 else head xs + sum' (tail xs)
+-- using pattern matching (see study 4)
+sum2 [] = 0
+sum2 (x:xs) = x + sum2 xs
 divisors n = [x | x <- [1..n], n `mod` x == 0]
